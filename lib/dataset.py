98fbdff5a8c5b690f2862de9dd3746ee6b3ce541
Leohoo27 <913621282@qq.com>
2020-03-05 14:53:19 +0800

    Update dataset.py

--- lib/dataset.py
+++ lib/dataset.py
@@ -29,17 +29,20 @@ import tensorflow as tf
 
 def read32(bytestream):
     """Read 4 bytes from bytestream as an unsigned 32-bit integer."""
+    # here, little confused
     dtype = np.dtype(np.uint32).newbyteorder(">")
+    print('******dtype: {0}*******'.format(dtype))
     return np.frombuffer(bytestream.read(4), dtype=dtype)[0]
 
 
 def check_image_file_header(filename):
     """Validate that filename corresponds to images for the MNIST dataset."""
-    with tf.gfile.Open(filename, "rb") as fptr:
+    with tf.io.gfile.GFile(filename, "rb") as fptr:
         magic = read32(fptr)
         read32(fptr)  # num_images, unused
         rows = read32(fptr)
         cols = read32(fptr)
+        # here why set these number: 2051, 28
         if magic != 2051:
             raise ValueError(
                 "Invalid magic number %d in MNIST file %s" % (magic, fptr.name)
@@ -53,7 +56,7 @@ def check_image_file_header(filename):
 
 def check_labels_file_header(filename):
     """Validate that filename corresponds to labels for the MNIST dataset."""
-    with tf.gfile.Open(filename, "rb") as fptr:
+    with tf.io.gfile.GFile(filename, "rb") as fptr:
         magic = read32(fptr)
         read32(fptr)  # num_items, unused
         if magic != 2049:
@@ -61,14 +64,13 @@ def check_labels_file_header(filename):
                 "Invalid magic number %d in MNIST file %s" % (magic, fptr.name)
             )
 
-
 def download(directory, filename):
     """Download (and unzip) a file from the MNIST dataset if not already done."""
     filepath = os.path.join(directory, filename)
-    if tf.gfile.Exists(filepath):
+    if tf.io.gfile.exists(filepath):
         return filepath
-    if not tf.gfile.Exists(directory):
-        tf.gfile.MakeDirs(directory)
+    if not tf.io.gfile.exists(directory):
+        tf.io.gfile.makedirs(directory)
     # CVDF mirror of http://yann.lecun.com/exdb/mnist/
     url = (
         "https://storage.googleapis.com/cvdf-datasets/mnist/"
@@ -78,7 +80,7 @@ def download(directory, filename):
     _, zipped_filepath = tempfile.mkstemp(suffix=".gz")
     print("Downloading %s to %s" % (url, zipped_filepath))
     urllib.request.urlretrieve(url, zipped_filepath)
-    with gzip.open(zipped_filepath, "rb") as f_in, tf.gfile.Open(
+    with gzip.open(zipped_filepath, "rb") as f_in, tf.io.gfile.GFile(
         filepath, "wb"
     ) as f_out:
         shutil.copyfileobj(f_in, f_out)
@@ -98,7 +100,7 @@ def dataset(directory, images_file, labels_file):
     def decode_image(image):
         """Decodes and normalizes the image."""
         # Normalize from [0, 255] to [0.0, 1.0]
-        image = tf.decode_raw(image, tf.uint8)
+        image = tf.io.decode_raw(image, tf.uint8)
         image = tf.cast(image, tf.float32)
         image = tf.reshape(image, [784])
         image = image / 255.0  # [0.0, 1.0]
@@ -108,13 +110,14 @@ def dataset(directory, images_file, labels_file):
 
     def decode_label(label):
         """Decodes and reshapes the label."""
-        label = tf.decode_raw(label, tf.uint8)  # tf.string -> [tf.uint8]
+        label = tf.io.decode_raw(label, tf.uint8)  # tf.string -> [tf.uint8]
         label = tf.reshape(label, [])  # label is a scalar
-        return tf.to_int32(label)
+        return tf.cast(label, tf.int32)
 
     images = tf.data.FixedLengthRecordDataset(
         images_file, 28 * 28, header_bytes=16
     ).map(decode_image)
+    #  fixed-length records from one or more binary files.
     labels = tf.data.FixedLengthRecordDataset(
         labels_file, 1, header_bytes=8
     ).map(decode_label)
