f690eedbc3cc60de8d5a8c17e63af6be6dd1229d
Leohoo27 <913621282@qq.com>
2020-03-05 14:46:58 +0800

    Update to Tensorflow 2.1

--- examples/nans/nan_fuzzer.py
+++ examples/nans/nan_fuzzer.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Fuzz a neural network to get a NaN."""
+# """Fuzz a neural network to get a NaN."""
+# """Change code to the tensorflow 2.1 version"""
+
 
 from __future__ import absolute_import
 from __future__ import division
@@ -28,25 +30,25 @@ from lib.mutation_functions import do_basic_mutations
 from lib.sample_functions import recent_sample_function
 
 
-tf.flags.DEFINE_string(
+tf.compat.v1.flags.DEFINE_string(
     "checkpoint_dir", None, "Dir containing checkpoints of model to fuzz."
 )
-tf.flags.DEFINE_integer(
+tf.compat.v1.flags.DEFINE_integer(
     "total_inputs_to_fuzz", 100, "Loops over the whole corpus."
 )
-tf.flags.DEFINE_integer(
+tf.compat.v1.flags.DEFINE_integer(
     "mutations_per_corpus_item", 100, "Number of times to mutate corpus item."
 )
-tf.flags.DEFINE_float(
+tf.compat.v1.flags.DEFINE_float(
     "ann_threshold",
     1.0,
     "Distance below which we consider something new coverage.",
 )
-tf.flags.DEFINE_integer("seed", None, "Random seed for both python and numpy.")
-tf.flags.DEFINE_boolean(
+tf.compat.v1.flags.DEFINE_integer("seed", None, "Random seed for both python and numpy.")
+tf.compat.v1.flags.DEFINE_boolean(
     "random_seed_corpus", False, "Whether to choose a random seed corpus."
 )
-FLAGS = tf.flags.FLAGS
+FLAGS = tf.compat.v1.flags.FLAGS
 
 
 def metadata_function(metadata_batches):
@@ -61,7 +63,7 @@ def metadata_function(metadata_batches):
 def objective_function(corpus_element):
     """Checks if the metadata is inf or NaN."""
     metadata = corpus_element.metadata
-    if all([np.isfinite(d).all() for d in metadata]):
+    if all([np.isfinite(d).all() for d in metadata]):  # if have inf, then return False
         return False
 
     tf.logging.info("Objective function satisfied: non-finite element found.")
@@ -72,7 +74,7 @@ def main(_):
     """Constructs the fuzzer and performs fuzzing."""
 
     # Log more
-    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
     # Set the seeds!
     if FLAGS.seed:
         random.seed(FLAGS.seed)
@@ -84,8 +86,8 @@ def main(_):
     )
     numpy_arrays = [[image, label]]
 
-    with tf.Session() as sess:
-
+    with tf.Graph().as_default() as g:  # change codes and it works
+        sess = tf.Session()  # here
         tensor_map = fuzz_utils.get_tensors_from_checkpoint(
             sess, FLAGS.checkpoint_dir
         )
@@ -93,13 +95,13 @@ def main(_):
         fetch_function = fuzz_utils.build_fetch_function(sess, tensor_map)
 
         size = FLAGS.mutations_per_corpus_item
-        mutation_function = lambda elt: do_basic_mutations(elt, size)
+        mutation_function = lambda elt: do_basic_mutations(elt, size)  # pram 1
         seed_corpus = seed_corpus_from_numpy_arrays(
             numpy_arrays, coverage_function, metadata_function, fetch_function
         )
         corpus = InputCorpus(
             seed_corpus, recent_sample_function, FLAGS.ann_threshold, "kdtree"
-        )
+        )  # pram 2
         fuzzer = Fuzzer(
             corpus,
             coverage_function,
@@ -108,16 +110,16 @@ def main(_):
             mutation_function,
             fetch_function,
         )
-        result = fuzzer.loop(FLAGS.total_inputs_to_fuzz)
+        result = fuzzer.loop(FLAGS.total_inputs_to_fuzz)  # iterations
         if result is not None:
-            tf.logging.info("Fuzzing succeeded.")
+            tf.compat.v1.logging.info("Fuzzing succeeded.")
             tf.logging.info(
                 "Generations to make satisfying element: %s.",
                 result.oldest_ancestor()[1],
             )
+            tf.compat.v1.logging.info("Elements for Crashes: {0}".format(result))
         else:
             tf.logging.info("Fuzzing failed to satisfy objective function.")
-
 
 if __name__ == "__main__":
     tf.app.run()
